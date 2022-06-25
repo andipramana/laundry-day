aa5d7cff0e5da46cd65eb10eac0fbd2fb0e8d441
andipramana <andipramana07@gmail.com>
2022-06-25 22:30:44 +0700

    Added icon

--- resources/views/components/common/logo.blade.php
+++ resources/views/components/common/logo.blade.php
@@ -1,4 +1,4 @@
 <a href="/" class="brand-link">
-    <img src="{{ asset('template/dist/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="{{ asset('template/dist/img/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Laundry Day</span>
 </a>
